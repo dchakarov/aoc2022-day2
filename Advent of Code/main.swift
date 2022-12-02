c46fad347b9ad1471621ce570e0dbc5c1a7d67fc
Dimitar Stoyanov Chakarov <dimitar.chakarov@justeattakeaway.com>
2022-12-02 12:46:46 +0000

    Part 2

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -13,12 +13,18 @@ func main() {
     let lines = inputString.components(separatedBy: "\n")
         .filter { !$0.isEmpty }
     
-    // Sample algorithm
     var totalScore = 0
     lines.forEach { line in
         let hands = parseLine(line)
         let roundScore = playRound(opponent: hands.opponent, me: hands.me)
-        print(roundScore)
+        totalScore += roundScore
+    }
+    print(totalScore)
+    
+    totalScore = 0
+    lines.forEach { line in
+        let (opponent, outcome) = parseLine2(line)
+        let roundScore = playRound(opponentHand: opponent, desiredOutcome: outcome)
         totalScore += roundScore
     }
     print(totalScore)
@@ -29,6 +35,11 @@ func parseLine(_ line: String) -> (opponent: Hand, me: Hand) {
     return (Hand(rawValue: result[0])!, Hand(rawValue: result[1])!)
 }
 
+func parseLine2(_ line: String) -> (opponent: OpponentHand, outcome: Outcome) {
+    let result = line.components(separatedBy: .whitespaces)
+    return (OpponentHand(rawValue: result[0])!, Outcome(rawValue: result[1])!)
+}
+
 enum Hand: String {
     case A
     case B
@@ -36,6 +47,61 @@ enum Hand: String {
     case X
     case Y
     case Z
+}
+
+enum OpponentHand: String {
+    case rock = "A"
+    case paper = "B"
+    case scissors = "C"
+}
+
+enum Outcome: String {
+    case lose = "X"
+    case draw = "Y"
+    case win = "Z"
+}
+
+func playRound(opponentHand: OpponentHand, desiredOutcome: Outcome) -> Int {
+    var playerHand: OpponentHand
+    
+    switch desiredOutcome {
+    case .draw:
+        playerHand = opponentHand
+    case .win:
+        switch opponentHand {
+        case .rock: playerHand = .paper
+        case .paper: playerHand = .scissors
+        case .scissors: playerHand = .rock
+        }
+    case .lose:
+        switch opponentHand {
+        case .rock: playerHand = .scissors
+        case .paper: playerHand = .rock
+        case .scissors: playerHand = .paper
+        }
+    }
+    
+    return calcPoints(hand: playerHand, outcome: desiredOutcome)
+}
+
+func calcPoints(hand: OpponentHand, outcome: Outcome) -> Int {
+    var points = 0
+    switch hand {
+    case .rock: points += 1
+    case .paper: points += 2
+    case .scissors: points += 3
+    }
+    
+    switch outcome {
+    case .win:
+        points += 6
+    case .draw:
+        points += 3
+    case .lose:
+        ()
+    }
+    
+    return points
 }
 
 func playRound(opponent: Hand, me: Hand) -> Int {
